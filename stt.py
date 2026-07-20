66d7eceb86da742c169c4f3c7b7e862675d28540
AlsonBasnet07 <basnetalson121@gmail.com>
2026-07-20 08:59:40 +0545

    taking the sample data mp3 and wrote the program to covert the data into the chunks

--- stt.py
+++ stt.py
@@ -1,6 +1,17 @@
 import whisper
+import json 
 model = whisper.load_model("large-v2")      
-result = model.transcribe(audio= "audios/sample.mp3",
+result = model.transcribe(audio= "audios/output.mp3",
                           language="hi",
-                          task="translate")
-print(result["text"])
+                          task="translate",
+                           word_timestamps=False)
+print(result["segments"])
+
+chunks=[]
+for segment in result["segments"]:
+    chunks.append({'start':segment["start"], "end":segment["end"],"text":segment["text"]})
+print(chunks)
+
+# this will put the text of the video in the json file
+with open ("outout.json","w") as f:
+    json.dump(chunks, f)
